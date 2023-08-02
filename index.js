01de4b8a0e412691d297482beac21aedb9d4641d
Yousra Khurshid <YousraKhurshid1@gmail.com>
2023-08-03 01:00:01 +0500

    FEAT:Create, Post, Get, Delete

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const categoryRouter = require('./API/Categories/Router')
+
+const port = process.env.SERVER_PORT || 3200
+
+app.use(express.json())
+app.use('/api', categoryRouter)
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
